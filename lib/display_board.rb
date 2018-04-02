7b9bafe7a8028e380af3c7b7c8ae05b987e3dc8f
Mike C <mikecronin92@gmail.com>
2018-04-02 02:10:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board()
   cells = ["   ","   ","   "]
-  2.times do {puts cells.join("|") + "\n-----------"}
+  2.times {puts cells.join("|") + "\n-----------"}
   puts cells.join("|")
 end
 
